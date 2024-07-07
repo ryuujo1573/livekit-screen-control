0ab27edf0ce3851793a15044f26065437e34f08f
ryuujo1573 <me@ryuujo.com>
2024-07-08 00:54:58 +0800

    chore(rust-agent): add submodule for rust-sdk reference

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,10 +1,12 @@
 use livekit::prelude::*;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIxMzI1MzU4NzUsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0MSIsIm5iZiI6MTcyMDI4NTQ3NSwic3ViIjoidGVzdGVlIiwidmlkZW8iOnsicm9vbSI6InRlc3Ryb29tIiwicm9vbUNyZWF0ZSI6dHJ1ZSwicm9vbUpvaW4iOnRydWV9fQ.G7lMSdbyYsU-LCmrkimWO4e5IX2j2HJsmpklWSolmmI";
     let server_url = "http://localhost:7880";
-    let (room, mut room_events) = Room::connect(server_url, token, RoomOptions::default()).await?;
+    let (room, mut room_events) = Room::connect(server_url, token, RoomOptions::default())
+        .await
+        .unwrap();
 
     while let Some(event) = room_events.recv().await {
         match event {
@@ -16,6 +18,4 @@ async fn main() -> Result<()> {
             _ => {}
         }
     }
-
-    Ok(())
 }
